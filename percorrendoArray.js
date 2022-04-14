24d7431bd67226e02b2c2fa5784ed33f4a83c565
mayara_Abreu <mayara.arj@gmail.com>
2022-04-14 09:33:20 -0300

    Criação do exercício percorrendo array

---
+++ percorrendoArray.js
@@ -0,0 +1,9 @@
+var listaDeFrutas = ["Uva", "Banana", "Manga", "Cajá", "Pinha"];
+var busca = "Cajá";
+
+for ( let i = 0; i < listaDeFrutas.length;i++){
+     busca == listaDeFrutas [i]
+     if (busca == listaDeFrutas[i]){
+         console.log ("Sim, temos a fruta "+ busca + " disponível")
+     }
+}
